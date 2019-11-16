39a853bc9d5aefe354b5b50fdaae937b0e4d5285
septjose <septjose@hotmail.com>
2019-11-16 01:24:41 -0600

    first commit

---
+++ WebCamSave/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WebCamSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Grabar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 79);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(488, 338);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(29, 31);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(377, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(548, 79);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(488, 338);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(936, 31);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Capturar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 443);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "subir al servidor";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(548, 443);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 44);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "descargar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 515);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WebCamSave/Form1.cs
@@ -0,0 +1,203 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WebCamSave
+{
+    public partial class Form1 : Form
+    {
+        private string ruta = @"C:\Users\José Alberto\Pictures\";
+        private bool HayDispositivos;
+        private FilterInfoCollection MisDispositivios;
+        private VideoCaptureDevice MiWebCam;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaDispositivos();
+        }
+
+        public void CargaDispositivos()
+        {
+            MisDispositivios = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MisDispositivios.Count > 0)
+            {
+                HayDispositivos = true;
+                for (int i = 0; i < MisDispositivios.Count; i++)
+                    comboBox1.Items.Add(MisDispositivios[i].Name.ToString());
+                comboBox1.Text = MisDispositivios[0].Name.ToString();
+            }
+            else
+                HayDispositivos = false;
+
+        }
+
+        private void CerrarWebCam()
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                MiWebCam.SignalToStop();
+                MiWebCam = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CerrarWebCam();
+            int i = comboBox1.SelectedIndex;
+            string NombreVideo = MisDispositivios[i].MonikerString;
+            MiWebCam = new VideoCaptureDevice(NombreVideo);
+            MiWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            MiWebCam.Start();
+        }
+
+        private void Capturando(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = Imagen;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CerrarWebCam();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                pictureBox2.Image = pictureBox1.Image;
+                pictureBox2.Image.Save(ruta + "yugi.jpg", ImageFormat.Jpeg);
+            }
+        }
+
+        public bool SubirFicheroStockFTP()
+        {
+            bool verdad;
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(
+                        ConfigurationManager.AppSettings["FTP_SERVER"].ToString() +
+                        ConfigurationManager.AppSettings["FTP_PATH"].ToString() +
+                        ConfigurationManager.AppSettings["NOMBRE_FICHERO"].ToString());
+
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                        ConfigurationManager.AppSettings["RUTA_FICHERO"],
+                        ConfigurationManager.AppSettings["NOMBRE_FICHERO"]);
+
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                request.Credentials = new NetworkCredential(
+                        ConfigurationManager.AppSettings["FTP_USER"].ToString(),
+                        ConfigurationManager.AppSettings["FTP_PASSWORD"].ToString());
+
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.KeepAlive = false;
+
+                using (var fileStream = File.OpenRead(filePath))
+                {
+                    using (var requestStream = request.GetRequestStream())
+                    {
+                        fileStream.CopyTo(requestStream);
+                        requestStream.Close();
+                    }
+                }
+
+                var response = (FtpWebResponse)request.GetResponse();
+
+                response.Close();
+                verdad = true;
+            }
+            catch (Exception ex)
+            {
+                //logger.Error("Error " + ex.Message + " " + ex.StackTrace);
+                verdad = false;
+                MessageBox.Show("Error " + ex.Message + " " + ex.StackTrace);
+                
+               
+            }
+            return verdad;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool subir = SubirFicheroStockFTP();
+            if(subir)
+            {
+                MessageBox.Show("SI se pudo");
+            }
+            else
+            {
+                MessageBox.Show("No se Pudo");
+            }
+        }
+
+        public bool downloadFile(string servidor, string usuario, string password, string archivoOrigen, string carpetaDestino, int bufferdes)
+        {
+            bool descargar;
+            try
+            {
+                FtpWebRequest reqFTP;
+                reqFTP = (FtpWebRequest)FtpWebRequest.Create(servidor + archivoOrigen);
+                reqFTP.Credentials = new NetworkCredential(usuario, password);
+                reqFTP.KeepAlive = false;
+                reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
+                reqFTP.UseBinary = true;
+                reqFTP.Proxy = null;
+                reqFTP.UsePassive = true;
+                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                FileStream writeStream = new FileStream(@carpetaDestino, FileMode.Create);
+                int Length = bufferdes;
+                Byte[] buffer = new Byte[Length];
+                int bytesRead = responseStream.Read(buffer, 0, Length);
+                while (bytesRead > 0)
+                {
+                    writeStream.Write(buffer, 0, bytesRead);
+                    bytesRead = responseStream.Read(buffer, 0, Length);
+                }
+                writeStream.Close();
+                response.Close();
+                descargar = true;
+            }
+            catch (WebException wEx)
+            {
+                descargar = false;
+                throw wEx;
+                
+            }
+            catch (Exception ex)
+            {
+                descargar = false;
+                throw ex;
+                
+
+            }
+            return descargar;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool descargo = downloadFile("ftp://jjdeveloperswdm.com/", "bonita_smile@jjdeveloperswdm.com", "bonita_smile", "yugi.jpg", @"C:\Users\José Alberto\Pictures\yugiohh\yugi.jpg", 10);
+            if(descargo)
+            {
+                MessageBox.Show(":)");
+            }
+            else
+            {
+                MessageBox.Show(":(");
+            }
+        }
+    }
+}
